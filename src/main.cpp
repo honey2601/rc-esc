93da76fbbf2185b72b5aa253021a97e70ae13802
DonPavlov <1541649+DonPavlov@users.noreply.github.com>
2019-09-07 20:00:03 +0200

    cleanup, fixes, improvements, no error for right stick anymore

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,25 @@
-// inspired by http://rcarduino.blogspot.com/2012/05/interfacing-rc-channels-to-l293d-motor.html
+// inspired by
+// http://rcarduino.blogspot.com/2012/05/interfacing-rc-channels-to-l293d-motor.html
 
-#include <Arduino.h>
-#include <EnableInterrupt.h>
-#include <EEPROM.h>
 #include "declarations.h"
+#include <Arduino.h>
+#include <EEPROM.h>
+#include <EnableInterrupt.h>
 
-static constexpr uint16_t rc_neutral = 1500;
-static constexpr uint16_t rc_max = 2000;
-static constexpr uint16_t rc_min = 1000;
-static constexpr uint16_t rc_deadband = 50;
+static constexpr uint32_t rc_neutral = 1500;
+static constexpr uint32_t rc_max = 2000;
+static constexpr uint32_t rc_min = 1000;
+static constexpr uint32_t rc_deadband = 50;
 
-static constexpr uint16_t rc_deadlow = 1150;
-static constexpr uint16_t rc_deadhigh = 1775;
-static constexpr uint16_t deadlo = rc_neutral - rc_deadband;
-static constexpr uint16_t deadhi = rc_neutral + rc_deadband;
+static constexpr uint32_t rc_deadlow = 1150;
+static constexpr uint32_t rc_deadhigh = 1850;
+static constexpr uint32_t deadlo = rc_neutral - rc_deadband;
+static constexpr uint32_t deadhi = rc_neutral + rc_deadband;
 static constexpr uint32_t correction_val = 3000;
 
-static constexpr int chR = 7;  // rechts hoch 1100 / runter 1820 // CH 1
+static constexpr int chR = 7; // rechts hoch 1100 / runter 1820 // CH 1
 // static constexpr int chLhori = 8z`; // links links 1820 / rechts 1100
-static constexpr int chL = 8;  // links hoch 1800 / runter 1100 // CH 3
+static constexpr int chL = 8; // links hoch 1800 / runter 1100 // CH 3
 // static constexpr int chRhori = 10; // rechts links 1100 / rechts 1820
 
 static constexpr uint8_t pwm_motor_1A = 5;
@@ -41,10 +42,12 @@ volatile uint32_t tempL;
 // define local functions
 void convertLeftStick();
 void convertRightStick();
+void convertRightStick2();
+
 void waitForTransmitter();
-void cleanup (channel_e stick);
+void cleanup(channel_e stick);
 void generatePwm(uint32_t value, channel_e channel, direction_e direction);
-uint8_t convertToPWMRange(uint32_t value); 
+uint8_t convertToPWMRange(uint32_t value);
 
 void setup();
 void loop();
@@ -82,8 +85,8 @@ void loop() {
   Serial.println(timerRRef);
 #endif
   if (timerLRef <= 1500) {
-     // left back
-     generatePwm(timerLRef, channel_e::LEFT, direction_e::BACKWARD);
+    // left back
+    generatePwm(timerLRef, channel_e::LEFT, direction_e::BACKWARD);
   } else {
     generatePwm(timerLRef, channel_e::LEFT, direction_e::FORWARD);
   }
@@ -96,19 +99,20 @@ void loop() {
 }
 
 void convertRightStick() {
-  if(digitalRead(chR) == HIGH) {
+  if (digitalRead(chR) == HIGH) {
     timerR = micros();
   }
-    tempR = micros() - timerR;
+  tempR = micros() - timerR;
+  if ((tempR >= rc_min) && (tempR <= rc_max)) {
     timerRRef = correction_val - tempR;
     cleanup(channel_e::RIGHT);
+  }
 }
 
 void convertLeftStick() {
-  if(digitalRead(chL) == HIGH) {
+  if (digitalRead(chL) == HIGH) {
     timerL = micros();
-  }
-  else {
+  } else {
     timerLRef = micros() - timerL;
     cleanup(channel_e::LEFT);
   }
@@ -118,66 +122,55 @@ void waitForTransmitter() {
   volatile uint32_t count = micros();
   volatile uint32_t count_start = micros();
   uint32_t count_end = 10000;
-  while(1) {
-    if (count > (count_start + count_end) ) {
+  while (1) {
+    if (count > (count_start + count_end)) {
       return;
     }
 
     if ((timerLRef < 1200) && (timerRRef > 1700)) {
       count = micros();
-      #if debug
+#if debug
       Serial.print("Init LRef:");
       Serial.print(timerLRef);
 
       Serial.print("Init RRef:");
       Serial.println(timerRRef);
-      #endif
-    }
-    else {
+#endif
+    } else {
       count_start = micros();
       count = micros();
-      #if debug
+#if debug
       Serial.print("Error LRef:");
       Serial.print(timerLRef);
 
       Serial.print("Error RRef:");
       Serial.println(timerRRef);
-      #endif
+#endif
     }
   }
 }
 
-// cleanup inputs regarding dead space for right stick mainly
 void cleanup(channel_e stick) {
-  if(stick == channel_e::LEFT) {
+  if (stick == channel_e::LEFT) {
     uint32_t temp = timerLRef;
-    if((temp > (deadlo)) && (temp < (deadhi))) {
+    if ((temp > (deadlo)) && (temp < (deadhi))) {
       timerLRef = rc_neutral;
-    }
-    else if ((temp > rc_deadhigh) && (temp < rc_max)) {
+    } else if ((temp > rc_deadhigh) && (temp < rc_max)) {
       timerLRef = rc_max;
-    }
-    else if ((temp < rc_deadlow) && (temp > rc_min)) {
+    } else if ((temp < rc_deadlow) && (temp > rc_min)) {
       timerLRef = rc_min;
     }
-    else if ((temp > rc_max) || (temp < rc_min)){ // no valid value set neutral
-      timerLRef = rc_neutral;
-    }
-  }
-  if(stick == channel_e::RIGHT) {
+    timerLRef = constrain(timerLRef, rc_min, rc_max);
+  } else if (stick == channel_e::RIGHT) {
     uint32_t temp = timerRRef;
-    if((temp > (deadlo)) && (temp < (deadhi))) {
+    if ((temp > (deadlo)) && (temp < (deadhi))) {
       timerRRef = rc_neutral;
-    }
-    else if ((temp > rc_deadhigh) && (temp < rc_max)) {
+    } else if ((temp > rc_deadhigh) && (temp < rc_max)) {
       timerRRef = rc_max;
-    }
-    else if ((temp < rc_deadlow) && (temp > rc_min)) {
+    } else if ((temp < rc_deadlow) && (temp > rc_min)) {
       timerRRef = rc_min;
     }
-    else if ((temp > rc_max) || (temp < rc_min)){ // no valid value set neutral
-      timerRRef = rc_neutral;
-    }
+    timerRRef = constrain(timerRRef, rc_min, rc_max);
   }
 }
 
@@ -185,7 +178,7 @@ void cleanup(channel_e stick) {
 void generatePwm(uint32_t value, channel_e channel, direction_e direction) {
   uint8_t val = convertToPWMRange(value);
   if (channel == channel_e::LEFT) {
-    if(direction == direction_e::FORWARD){
+    if (direction == direction_e::FORWARD) {
       // disable other direction
       analogWrite(pwm_motor_1B, 0);
       analogWrite(pwm_motor_1A, val);
@@ -195,7 +188,7 @@ void generatePwm(uint32_t value, channel_e channel, direction_e direction) {
       analogWrite(pwm_motor_1B, val);
     }
   } else {
-    if(direction == direction_e::FORWARD){
+    if (direction == direction_e::FORWARD) {
       // disable other direction
       analogWrite(pwm_motor_2B, 0);
       analogWrite(pwm_motor_2A, val);
@@ -210,10 +203,10 @@ void generatePwm(uint32_t value, channel_e channel, direction_e direction) {
 uint8_t convertToPWMRange(uint32_t value) {
   uint32_t val = 0;
   double result = 0.0;
-  if(value < rc_neutral) {
-    val = 3000 - value;  //  3000 - 1200 = 1800
+  if (value < rc_neutral) {
+    val = 3000 - value;     //  3000 - 1200 = 1800
     val = val - rc_neutral; // 1800 - 1500 = 300
-  } else if(value > rc_neutral) {
+  } else if (value > rc_neutral) {
     val = value - rc_neutral; // 1800 - 1500 = 300
   } else if (value == rc_neutral) {
     return 0;
