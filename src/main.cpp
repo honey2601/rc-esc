857b179d856befb569aaf2eead04f9c360512000
DonPavlov <1541649+DonPavlov@users.noreply.github.com>
2020-05-23 14:54:31 +0200

    update stuff for new motor driver

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 // inspired by
 // http://rcarduino.blogspot.com/2012/05/interfacing-rc-channels-to-l293d-motor.html
 
-#include "declarations.h"
+// ecksteinimg.de/Datasheet/Pololu/PO2135.pdf
+
+#include "declarations.h"s
 #include <Arduino.h>
 #include <EEPROM.h>
 #include <EnableInterrupt.h>
@@ -22,13 +24,11 @@ static constexpr int chR = 7; // rechts hoch 1100 / runter 1820 // CH 1
 static constexpr int chL = 8; // links hoch 1800 / runter 1100 // CH 3
 // static constexpr int chRhori = 10; // rechts links 1100 / rechts 1820
 
-static constexpr uint8_t pwm_motor_1A = 5;
-static constexpr uint8_t pwm_motor_1B = 6;
-static constexpr uint8_t pwm_motor_2A = 9;
-static constexpr uint8_t pwm_motor_2B = 10;
+static constexpr uint8_t pwm_motor_1 = 5;
+static constexpr uint8_t pwm_motor_2 = 9;
 
-static constexpr uint8_t enable_motor_1 = 14;
-static constexpr uint8_t enable_motor_2 = 15;
+static constexpr uint8_t phase_motor_1 = 14;
+static constexpr uint8_t phase_motor_2 = 15;
 
 volatile uint32_t timerL;
 volatile uint32_t timerR;
@@ -52,18 +52,17 @@ uint8_t convertToPWMRange(uint32_t value);
 void setup();
 void loop();
 
-void setup() {
+void setup()
+{
   // Set input pins
   pinMode(chR, INPUT);
   pinMode(chL, INPUT);
 
   // Setup output pins
-  pinMode(pwm_motor_1A, OUTPUT);
-  pinMode(pwm_motor_1B, OUTPUT);
-  pinMode(pwm_motor_2A, OUTPUT);
-  pinMode(pwm_motor_2B, OUTPUT);
-  pinMode(enable_motor_1, OUTPUT);
-  pinMode(enable_motor_2, OUTPUT);
+  pinMode(pwm_motor_1, OUTPUT);
+  pinMode(pwm_motor_1, OUTPUT);
+  pinMode(phase_motor_1, OUTPUT);
+  pinMode(phase_motor_2, OUTPUT);
 
   enableInterrupt(chR, convertRightStick, CHANGE);
   enableInterrupt(chL, convertLeftStick, CHANGE);
@@ -72,62 +71,81 @@ void setup() {
   delay(5000);
   waitForTransmitter();
   Serial.begin(115200);
-  digitalWrite(enable_motor_1, HIGH);
-  digitalWrite(enable_motor_2, HIGH);
+  generatePwm(1750, channel_e::LEFT, direction_e::FORWARD);
+  delay(5000);
+  generatePwm(1500, channel_e::LEFT, direction_e::FORWARD);
 }
 
-void loop() {
-#if debug
-  Serial.print("LRef:");
-  Serial.print(timerLRef);
+void loop()
+{
+  // #if debug
+  //   Serial.print("LRef:");
+  //   Serial.print(timerLRef);
 
-  Serial.print("RRef:");
-  Serial.println(timerRRef);
-#endif
-  if (timerLRef <= 1500) {
-    // left back
-    generatePwm(timerLRef, channel_e::LEFT, direction_e::BACKWARD);
-  } else {
-    generatePwm(timerLRef, channel_e::LEFT, direction_e::FORWARD);
-  }
-  if (timerRRef <= 1500) {
-    // right back
-    generatePwm(timerRRef, channel_e::RIGHT, direction_e::BACKWARD);
-  } else {
-    generatePwm(timerRRef, channel_e::RIGHT, direction_e::FORWARD);
-  }
+  //   Serial.print("RRef:");
+  //   Serial.println(timerRRef);
+  // #endif
+  //   if (timerLRef <= 1500)
+  //   {
+  //     // left back
+  //     generatePwm(timerLRef, channel_e::LEFT, direction_e::BACKWARD);
+  //   }
+  //   else
+  //   {
+  //     generatePwm(timerLRef, channel_e::LEFT, direction_e::FORWARD);
+  //   }
+  //   if (timerRRef <= 1500)
+  //   {
+  //     // right back
+  //     generatePwm(timerRRef, channel_e::RIGHT, direction_e::BACKWARD);
+  //   }
+  //   else
+  //   {
+  //     generatePwm(timerRRef, channel_e::RIGHT, direction_e::FORWARD);
+  //   }
 }
 
-void convertRightStick() {
-  if (digitalRead(chR) == HIGH) {
+void convertRightStick()
+{
+  if (digitalRead(chR) == HIGH)
+  {
     timerR = micros();
   }
   tempR = micros() - timerR;
-  if ((tempR >= rc_min) && (tempR <= rc_max)) {
+  if ((tempR >= rc_min) && (tempR <= rc_max))
+  {
     timerRRef = correction_val - tempR;
     cleanup(channel_e::RIGHT);
   }
 }
 
-void convertLeftStick() {
-  if (digitalRead(chL) == HIGH) {
+void convertLeftStick()
+{
+  if (digitalRead(chL) == HIGH)
+  {
     timerL = micros();
-  } else {
+  }
+  else
+  {
     timerLRef = micros() - timerL;
     cleanup(channel_e::LEFT);
   }
 }
 
-void waitForTransmitter() {
+void waitForTransmitter()
+{
   volatile uint32_t count = micros();
   volatile uint32_t count_start = micros();
   uint32_t count_end = 10000;
-  while (1) {
-    if (count > (count_start + count_end)) {
+  while (1)
+  {
+    if (count > (count_start + count_end))
+    {
       return;
     }
 
-    if ((timerLRef < 1200) && (timerRRef > 1700)) {
+    if ((timerLRef < 1200) && (timerRRef > 1700))
+    {
       count = micros();
 #if debug
       Serial.print("Init LRef:");
@@ -136,7 +154,9 @@ void waitForTransmitter() {
       Serial.print("Init RRef:");
       Serial.println(timerRRef);
 #endif
-    } else {
+    }
+    else
+    {
       count_start = micros();
       count = micros();
 #if debug
@@ -150,24 +170,38 @@ void waitForTransmitter() {
   }
 }
 
-void cleanup(channel_e stick) {
-  if (stick == channel_e::LEFT) {
+void cleanup(channel_e stick)
+{
+  if (stick == channel_e::LEFT)
+  {
     uint32_t temp = timerLRef;
-    if ((temp > (deadlo)) && (temp < (deadhi))) {
+    if ((temp > (deadlo)) && (temp < (deadhi)))
+    {
       timerLRef = rc_neutral;
-    } else if ((temp > rc_deadhigh) && (temp < rc_max)) {
+    }
+    else if ((temp > rc_deadhigh) && (temp < rc_max))
+    {
       timerLRef = rc_max;
-    } else if ((temp < rc_deadlow) && (temp > rc_min)) {
+    }
+    else if ((temp < rc_deadlow) && (temp > rc_min))
+    {
       timerLRef = rc_min;
     }
     timerLRef = constrain(timerLRef, rc_min, rc_max);
-  } else if (stick == channel_e::RIGHT) {
+  }
+  else if (stick == channel_e::RIGHT)
+  {
     uint32_t temp = timerRRef;
-    if ((temp > (deadlo)) && (temp < (deadhi))) {
+    if ((temp > (deadlo)) && (temp < (deadhi)))
+    {
       timerRRef = rc_neutral;
-    } else if ((temp > rc_deadhigh) && (temp < rc_max)) {
+    }
+    else if ((temp > rc_deadhigh) && (temp < rc_max))
+    {
       timerRRef = rc_max;
-    } else if ((temp < rc_deadlow) && (temp > rc_min)) {
+    }
+    else if ((temp < rc_deadlow) && (temp > rc_min))
+    {
       timerRRef = rc_min;
     }
     timerRRef = constrain(timerRRef, rc_min, rc_max);
@@ -175,43 +209,59 @@ void cleanup(channel_e stick) {
 }
 
 // alternatively occr1a etc.. check out atmega32u4 timers
-void generatePwm(uint32_t value, channel_e channel, direction_e direction) {
+void generatePwm(uint32_t value, channel_e channel, direction_e direction)
+{
   uint8_t val = convertToPWMRange(value);
-  if (channel == channel_e::LEFT) {
-    if (direction == direction_e::FORWARD) {
-      // disable other direction
-      analogWrite(pwm_motor_1B, 0);
-      analogWrite(pwm_motor_1A, val);
-    } else {
-      // disable other direction
-      analogWrite(pwm_motor_1A, 0);
-      analogWrite(pwm_motor_1B, val);
+  if (channel == channel_e::LEFT)
+  {
+    if (direction == direction_e::FORWARD)
+    {
+      // phase to forward
+      digitalWrite(phase_motor_1, 0);
+      analogWrite(pwm_motor_1, val);
     }
-  } else {
-    if (direction == direction_e::FORWARD) {
-      // disable other direction
-      analogWrite(pwm_motor_2B, 0);
-      analogWrite(pwm_motor_2A, val);
-    } else {
-      // disable other direction
-      analogWrite(pwm_motor_2A, 0);
-      analogWrite(pwm_motor_2B, val);
+    else
+    {
+      // phase to backward
+      analogWrite(phase_motor_1, 1);
+      analogWrite(pwm_motor_1, val);
+    }
+  }
+  else
+  {
+    if (direction == direction_e::FORWARD)
+    {
+      // phase to forward
+      analogWrite(phase_motor_2, 0);
+      analogWrite(pwm_motor_2, val);
+    }
+    else
+    {
+      // phase to backward
+      analogWrite(phase_motor_2, 1);
+      analogWrite(pwm_motor_2, val);
     }
   }
 }
 
-uint8_t convertToPWMRange(uint32_t value) {
+uint8_t convertToPWMRange(uint32_t value)
+{
   uint32_t val = 0;
   double result = 0.0;
-  if (value < rc_neutral) {
+  if (value < rc_neutral)
+  {
     val = 3000 - value;     //  3000 - 1200 = 1800
     val = val - rc_neutral; // 1800 - 1500 = 300
-  } else if (value > rc_neutral) {
+  }
+  else if (value > rc_neutral)
+  {
     val = value - rc_neutral; // 1800 - 1500 = 300
-  } else if (value == rc_neutral) {
+  }
+  else if (value == rc_neutral)
+  {
     return 0;
   }
-  result = ((val / 500) * 175)+ 75; // motor only drives at high pwms give it a boost of 75
+  result = ((val / 500) * 175) + 75; // motor only drives at high pwms give it a boost of 75
 
   return static_cast<uint8_t>(result);
 }
